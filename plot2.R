43f2646b691b049544d1ff0915d36229af772de3
IVAN FLORES VITELLI <ivanjfv@users.noreply.github.com>
2015-01-10 06:41:36 -0430

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -8,6 +8,8 @@ data<-subset(mydata,Date=="2/2/2007" | Date=="1/2/2007")
 data$newdate<-paste(data$Date, data$Time, sep=" ")
 
 #Create Graph
+#The system is installed in Spanish , therefore , days of the week are written also in Spanish
+#Jue=Thu, Vie=Fri, Sab=Sat
 png(file = "plot2.png", width = 480, height = 480)
 with(data,plot(strptime(data$newdate, "%d/%m/%Y %T" ),Global_active_power,type="l",xlab="",ylab="Global Active Power (kilowatts)"))
 dev.off()
